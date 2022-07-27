b40211817f898c8ed75aa88e25a8f3ea200b347f
Chris Pierre Paul <cp20fl@fsu.edu>
2022-07-27 12:51:24 -0400

    Updated functionality of services to communicate with API

--- Library.Standard.Product/Services/InventoryService.cs
+++ Library.Standard.Product/Services/InventoryService.cs
@@ -1,5 +1,5 @@
-﻿using Library.TaskManagement.Models;
-using Library.TaskManagement.Utility;
+﻿using Library.Standard.Products.Utility;
+using Library.TaskManagement.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,15 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Library.TaskManagement.Services
 {
     public class InventoryService
     {
-        private string persistPath 
+        private string persistPath
             = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}";
         private static InventoryService instance;
         private List<Product> inventory;
+
         public List<Product> Inventory
         {
             get { return inventory; }
@@ -35,13 +35,23 @@ namespace Library.TaskManagement.Services
         }
         private InventoryService()
         {
+            /* Assignment 3 */
             inventory = new List<Product>();
+
+
+            /*Assignment 4 */
+            var productsJson = new WebRequestHandler().Get("http://localhost:5048/Inventory").Result;
+            if (productsJson != null)
+            {
+                inventory = JsonConvert.DeserializeObject<List<Product>>(productsJson);
+            }
+
         }
 
         public void AddOrUpdate(Product product)
         {
-            Console.WriteLine($"Current product is {product}");
-
+            //Assignment 3
+            /*
             if (product.Id == 0)
             {
                 if (inventory.Any())
@@ -56,30 +66,56 @@ namespace Library.TaskManagement.Services
 
             if (!inventory.Any(i => i.Id == product.Id))
             { //not in inventory add product
-                if (product is ProductByQuantity) 
+                if (product is ProductByQuantity)
                 {
                     var q = product as ProductByQuantity;
                     q.UpdateI();
                     product = q;
                 }
-                else if (product is ProductByWeight) 
+                else if (product is ProductByWeight)
                 {
                     var w = product as ProductByWeight;
                     w.UpdateI();
                     product = w;
                 }
-                
+
                 inventory.Add(product);
             }
+            */
+
+            //Assignment 4
+            var response = new WebRequestHandler().Post("http://localhost:5048/Inventory/AddOrUpdate", product).Result;
+            var newP = JsonConvert.DeserializeObject<Product>(response);
+
+            var oldVersion = inventory.FirstOrDefault(i => i.Id == newP.Id);
+            if (oldVersion != null)
+            {
+                var index = inventory.IndexOf(oldVersion);
+                inventory.RemoveAt(index);
+                inventory.Insert(index, newP);
+                int num = inventory.Count();
+            }
+            else
+            {
+                inventory.Add(newP);
+            }
         }
+
         public void Delete(int id)
         {
+            var response = new WebRequestHandler().Get($"http://localhost:5048/Inventory/Delete/{id}");
             var ProductToDelete = inventory.FirstOrDefault(i => i.Id == id);
             if (ProductToDelete != null)
             {
                 Console.WriteLine($"Deleting {ProductToDelete}");
                 inventory.Remove(ProductToDelete);
             }
+        }
+
+        public void MarkBOGO(Product p) 
+        {
+            p.BG = true;
+            AddOrUpdate(p);
         }
 
         public IEnumerable<Product> SortIBP()
@@ -95,8 +131,11 @@ namespace Library.TaskManagement.Services
             return inventory;
         }
 
+        /* Assignment 3 Save
         public void Save(string fileName = null)
-        { //NEFARIOUS CODE
+        {
+            
+            //NEFARIOUS CODE
             if (string.IsNullOrEmpty(fileName))
             {
                 fileName = $"{persistPath}\\SaveData.json";
@@ -109,9 +148,18 @@ namespace Library.TaskManagement.Services
                 , new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
             File.WriteAllText(fileName, todosJson);
         }
+        */
 
+        public void Save() 
+        {
+            var savecart = new WebRequestHandler().Post($"http://localhost:5048/Inventory/Save", inventory).Result;
+            if (savecart != null) { inventory = JsonConvert.DeserializeObject<List<Product>>(savecart); }
+        }
+
+        /* Assignment 3 Load 
         public void Load(string fileName = null)
-        {  //NEFARIOUS CODE
+        {
+            //NEFARIOUS CODE
             if (string.IsNullOrEmpty(fileName))
             {
                 fileName = $"{persistPath}\\SaveData.json";
@@ -126,5 +174,12 @@ namespace Library.TaskManagement.Services
                 (ProductJson, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All })
                 ?? new List<Product>();
         }
+            */
+
+        public void Load()
+        {
+            var loadcart = new WebRequestHandler().Get($"http://localhost:5048/Inventory/Load").Result;
+            if (loadcart != null) { inventory = JsonConvert.DeserializeObject<List<Product>>(loadcart); }
+        }
     }
-}
+} 
--- Library.Standard.Product/Services/ProductService.cs
+++ Library.Standard.Product/Services/ProductService.cs
@@ -1,34 +1,35 @@
-﻿using Library.TaskManagement.Models;
-using Library.TaskManagement.Utility;
+﻿using Library.Standard.Products.Utility;
+using Library.TaskManagement.Services;
+using Library.TaskManagement.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library.TaskManagement.Services
 {
     public class ProductService
     {
+        private InventoryService IS = InventoryService.Current;
         private List<Product> Cart; // cart list
 
         private string CurrentCart { get; set; }
         private List<string> cartnames;
-        private string persistPath= $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}";
+        private string persistPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}";
         private readonly string CNPath = "CartList";
 
         public string ccart
         { get { return CurrentCart; } }
+         
         public List<string> CartNames
-        { get { return cartnames; }}
+        { get { return cartnames; } }
 
         public List<Product> Carts
         { get { return Cart; } }
 
         public List<Product> Products
-        { get { return InventoryService.Current.Inventory; } }
+        { get { return IS.Inventory; } }
 
         private static ProductService current;
         public static ProductService Current
@@ -47,10 +48,17 @@ namespace Library.TaskManagement.Services
         {
             Cart = new List<Product>(); //Creates cart list
             cartnames = new List<string>();
+
+            var cjson = new WebRequestHandler().Get("http://localhost:5048/Cart").Result;
+            if (cjson != null)
+            {
+                Cart = JsonConvert.DeserializeObject<List<Product>>(cjson);
+            }
         }
 
         public void Delete(int id)
         {
+            var response = new WebRequestHandler().Get($"http://localhost:5048/Cart/Delete/{id}");
             var ProductToDelete = Cart.FirstOrDefault(t => t.Id == id);
 
             if (ProductToDelete is ProductByQuantity)
@@ -60,18 +68,16 @@ namespace Library.TaskManagement.Services
                 if (Quantity != null)
                 {
                     // remove product from cart while updating both Cart, Inventory quantity and total price
-                    if (Products.Contains(Quantity) == true)
+                    if (Cart.Contains(Quantity) == true)
                     {
-                        Quantity.InventoryQuantity += Quantity.CartQuantity;
-                        Quantity.CartQuantity -= Quantity.CartQuantity;
-                        Quantity.Calculate();
+                        Quantity.RemoveCQ();
                     }
                     else
                     if (Products.Contains(Quantity) == false)
                     {
                         Quantity.UpdateC();
-                        Products.Add(Quantity);
                     }
+                    IS.AddOrUpdate(Quantity);
                     Cart.Remove(Quantity);
                 }
             }
@@ -82,19 +88,16 @@ namespace Library.TaskManagement.Services
                 if (Weight != null)
                 {
                     // remove product from cart while updating both Cart and Inventory quantity
-                    if(Products.Contains(Weight) == true)
-                    { //if not in inventory
-                        Weight.IWeight += Weight.CWeight;
-                        Weight.CWeight -= Weight.CWeight;
-                        Weight.Calculate();
+                    if (Cart.Contains(Weight) == true)
+                    { 
+                        Weight.RemoveCW();
                     }
                     else
                     if (Products.Contains(Weight) == false)
-                    { //if in inventory
+                    {
                         Weight.UpdateC();
-                        Products.Add(Weight);
                     }
-
+                    IS.AddOrUpdate(Weight);
                     Cart.Remove(Weight);
                 }
             }
@@ -129,15 +132,13 @@ namespace Library.TaskManagement.Services
                 {
                     if (Q.InventoryQuantity >= quantity)
                     {
-                        Q.InventoryQuantity -= quantity;
-                        Q.CartQuantity += quantity;
-                        Q.Calculate();
+                        Q.AddCQ(quantity);
 
                         if (!Cart.Contains(product) && Q.WithinStock)
                         { //if quantity is less than or equal to Inventoryquantity && not in carts,
-                            // update values and adds to cart
-                            
-                            Cart.Add(Q);
+                          // update values and adds to cart
+
+                            AddOrUpdate(Q);
                         }
                     }
 
@@ -148,33 +149,290 @@ namespace Library.TaskManagement.Services
         public void AddtoC(Product product, double weight)
         { //adds product from Inventory to Cart while removing completely from Inventory
 
-                 if (product is ProductByWeight)
-                 {
-                    if (weight <= 0)
+            if (product is ProductByWeight)
+            {
+                if (weight <= 0)
+                {
+                    return;
+                }
+
+                var W = product as ProductByWeight;
+
+                if (W != null)
+                {
+                    W.IWeight = Math.Round(W.IWeight, 2);
+                    W.CWeight = Math.Round(W.CWeight, 2);
+                    if (W.IWeight >= weight)
                     {
-                        return;
-                    }
+                        W.AddCW(weight);
 
-                    var W = product as ProductByWeight;
-
-                    if (W != null)
-                    {
-                        W.IWeight = Math.Round(W.IWeight, 2);
-                        W.CWeight = Math.Round(W.CWeight, 2);
-                        if (W.IWeight >= weight)
-                        {
-                            W.IWeight -= weight;
-                            W.CWeight += weight;
-                            W.Calculate();
-
-                            if (!Cart.Contains(product) && W.WithinStock)
-                            { //if quantity is less than or equal to Inventoryquantity && not in carts,
-                                // update values and adds to cart
-                                Cart.Add(W);
-                            }
+                        if (!Cart.Contains(product) && W.WithinStock)
+                        { //if quantity is less than or equal to Inventoryquantity && not in carts,
+                          // update values and adds to cart
+                            AddOrUpdate(W);
                         }
                     }
                 }
+            }
+        }
+
+        public void AddOrUpdate(Product product)
+        {
+            /* Assignment 3
+            Console.WriteLine($"Current product is {product}");
+
+            if (product.Id == 0)
+            {
+                if (Cart.Any())
+                { //if list has elements add to product id
+                    product.Id = Cart.Select(i => i.Id).Max() + 1;
+                }
+                else
+                { //if first element make id 1
+                    product.Id = 1;
+                }
+            }
+
+            if (!Cart.Any(i => i.Id == product.Id))
+            { //not in inventory add product
+                Cart.Add(product);
+            }
+            */
+
+            //Assignment 4
+            var response = new WebRequestHandler().Post("http://localhost:5048/Cart/AddOrUpdate", product).Result;
+            var newP = JsonConvert.DeserializeObject<Product>(response);
+
+            var oldVersion = Cart.FirstOrDefault(i => i.Id == newP.Id);
+            if (oldVersion != null)
+            {
+                var index = Cart.IndexOf(oldVersion);
+                Cart.RemoveAt(index);
+                Cart.Insert(index, newP);
+            }
+            else
+            {
+                Cart.Add(newP);
+            }
+            //Updates inventory version of product
+            IS.AddOrUpdate(product);
+        }
+
+        public double calcsubt()
+        {
+            double subtotal = 0;
+            foreach (Product p in Cart)
+            {
+                subtotal += p.TotalPrice;
+            }
+            return Math.Round(subtotal, 2);
+        }
+
+        public double calctax()
+        {
+            return Math.Round((calcsubt() * 0.07), 2);
+        }
+
+        public double calct()
+        {
+            return Math.Round(calcsubt() + calctax(), 2);
+        }
+
+        public void Save(string fileName = null)
+        {
+            CurrentCart = fileName;
+            cartnames.Add(CurrentCart);
+            var savecart = new WebRequestHandler().Get($"http://localhost:5048/Cart/Save/{fileName}").Result;
+            if (savecart != null) { Cart = JsonConvert.DeserializeObject<List<Product>>(savecart); }
+
+        }
+
+        public void Load(string fileName = null)
+        {
+            CurrentCart = fileName;
+            var loadcart = new WebRequestHandler().Get($"http://localhost:5048/Cart/Load/{fileName}").Result;
+            Cart = JsonConvert.DeserializeObject<List<Product>>(loadcart);
+        }
+
+        public void SaveCarts()
+        {
+            var savecarts = new WebRequestHandler().Post($"http://localhost:5048/Cart/SaveCarts", cartnames).Result;
+            cartnames = JsonConvert.DeserializeObject<List<string>>(savecarts);
+        }
+
+        public List<string> LoadCarts()
+        {
+            var loadcarts = new WebRequestHandler().Get($"http://localhost:5048/Cart/LoadCarts").Result;
+
+            if (loadcarts != null) { cartnames = JsonConvert.DeserializeObject<List<string>>(loadcarts); }
+
+            return cartnames;
+        }
+    }
+}
+
+/* Assignment 3 Version
+namespace Library.TaskManagement.Services
+{
+    public class ProductService
+    {
+        private InventoryService IS = InventoryService.Current;
+        private List<Product> Cart; // cart list
+
+        private string CurrentCart { get; set; }
+        private List<string> cartnames;
+        private string persistPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}";
+        private readonly string CNPath = "CartList";
+
+        public string ccart
+        { get { return CurrentCart; } }
+        public List<string> CartNames
+        { get { return cartnames; } }
+
+        public List<Product> Carts
+        { get { return Cart; } }
+
+        public List<Product> Products
+        { get { return IS.Inventory; } }
+
+        private static ProductService current;
+        public static ProductService Current
+        {
+            get
+            {
+                if (current == null)
+                {
+                    current = new ProductService();
+                }
+                return current;
+            }
+        }
+
+        private ProductService()
+        {
+            Cart = new List<Product>(); //Creates cart list
+            cartnames = new List<string>();
+        }
+
+        public void Delete(int id)
+        {
+            var ProductToDelete = Cart.FirstOrDefault(t => t.Id == id);
+
+            if (ProductToDelete is ProductByQuantity)
+            {
+                var Quantity = ProductToDelete as ProductByQuantity;
+
+                if (Quantity != null)
+                {
+                    // remove product from cart while updating both Cart, Inventory quantity and total price
+                    if (Cart.Contains(Quantity) == true)
+                    {
+                        Quantity.RemoveCQ();
+                    }
+                    else
+                    if (Products.Contains(Quantity) == false)
+                    {
+                        Quantity.UpdateC();
+                        //Products.Add(Quantity);
+                    }
+                    IS.AddOrUpdate(Quantity);
+                    Cart.Remove(Quantity);
+                }
+            }
+            else if (ProductToDelete is ProductByWeight)
+            {
+                var Weight = ProductToDelete as ProductByWeight;
+
+                if (Weight != null)
+                {
+                    // remove product from cart while updating both Cart and Inventory quantity
+                    if (Products.Contains(Weight) == true)
+                    { //if not in inventory
+                        Weight.RemoveCW();
+                    }
+                    else
+                    if (Products.Contains(Weight) == false)
+                    { //if in inventory
+                        Weight.UpdateC();
+                        //Products.Add(Weight);
+                    }
+                    IS.AddOrUpdate(Weight);
+                    Cart.Remove(Weight);
+                }
+            }
+        }
+
+        public IEnumerable<Product> SortCBTP()
+        {
+            Console.WriteLine("Sorted by TotalPrice");
+            Cart = Cart.OrderBy(i => i.TotalPrice).ToList();
+            return Cart;
+        }
+
+        public IEnumerable<Product> SortCBN()
+        {
+            Console.WriteLine("Sorted by Name");
+            Cart = Cart.OrderBy(i => i.Name).ToList();
+            return Cart;
+        }
+
+        public void AddtoC(Product product, int quantity)
+        { //adds product from Inventory to Cart while removing completely from Inventory
+
+            if (product is ProductByQuantity)
+            {
+                if (quantity <= 0)
+                {
+                    return;
+                }
+                var Q = product as ProductByQuantity;
+
+                if (Q != null)
+                {
+                    if (Q.InventoryQuantity >= quantity)
+                    {
+                        Q.AddCQ(quantity);
+
+                        if (!Cart.Contains(product) && Q.WithinStock)
+                        { //if quantity is less than or equal to Inventoryquantity && not in carts,
+                          // update values and adds to cart
+
+                            AddOrUpdate(Q);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public void AddtoC(Product product, double weight)
+        { //adds product from Inventory to Cart while removing completely from Inventory
+
+            if (product is ProductByWeight)
+            {
+                if (weight <= 0)
+                {
+                    return;
+                }
+
+                var W = product as ProductByWeight;
+
+                if (W != null)
+                {
+                    W.IWeight = Math.Round(W.IWeight, 2);
+                    W.CWeight = Math.Round(W.CWeight, 2);
+                    if (W.IWeight >= weight)
+                    {
+                        W.AddCW(weight);
+
+                        if (!Cart.Contains(product) && W.WithinStock)
+                        { //if quantity is less than or equal to Inventoryquantity && not in carts,
+                          // update values and adds to cart
+                            AddOrUpdate(W);
+                        }
+                    }
+                }
+            }
         }
         public void AddOrUpdate(Product product)
         {
@@ -198,7 +456,7 @@ namespace Library.TaskManagement.Services
             }
         }
 
-        public double calcsubt() 
+        public double calcsubt()
         {
             double subtotal = 0;
             foreach (Product p in Cart)
@@ -208,12 +466,12 @@ namespace Library.TaskManagement.Services
             return Math.Round(subtotal, 2);
         }
 
-        public double calctax() 
+        public double calctax()
         {
             return Math.Round((calcsubt() * 0.07), 2);
         }
 
-        public double calct() 
+        public double calct()
         {
             return Math.Round(calcsubt() + calctax(), 2);
         }
@@ -254,12 +512,12 @@ namespace Library.TaskManagement.Services
                 ?? new List<Product>();
         }
 
-        public void SaveCarts() 
+        public void SaveCarts()
         {
             var fileName = $"{persistPath}\\{CNPath}.json";
             var CartJson = JsonConvert.SerializeObject(cartnames
                 , new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
-                File.WriteAllText(fileName, CartJson);
+            File.WriteAllText(fileName, CartJson);
         }
 
         public void LoadCarts()
@@ -271,4 +529,5 @@ namespace Library.TaskManagement.Services
                 ?? new List<string>();
         }
     }
-}
+} 
+*/
